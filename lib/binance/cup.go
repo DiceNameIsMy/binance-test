2ff746cc7aa9e13cdbf1f9284d97eadc7a630a16
Dice <dicenameismy@gmail.com>
2021-09-10 19:32:55 +0600

    cup bugfix

--- lib/binance/cup.go
+++ lib/binance/cup.go
@@ -17,19 +17,19 @@ func (c *Cup) cut_data() {
 	c.Asks = c.Asks[:15]
 }
 
-func (c Cup) get_total_asks() string {
+func (c *Cup) get_total_asks() string {
 	float_total := c.get_totals(c.Asks)
 	total_ask := fmt.Sprintf("%f", float_total)
 	return total_ask
 }
 
-func (c Cup) get_total_bids() string {
+func (c *Cup) get_total_bids() string {
 	float_total := c.get_totals(c.Bids)
 	total_ask := fmt.Sprintf("%f", float_total)
 	return total_ask
 }
 
-func (c Cup) get_totals(data [][2]string) float64 {
+func (c *Cup) get_totals(data [][2]string) float64 {
 	var total float64
 	for _, val := range data {
 		q, err := strconv.ParseFloat(val[0], 64)
