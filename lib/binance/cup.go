4ef43765678ddcd545656075c12ec15aabbb1e8a
Dice <dicenameismy@gmail.com>
2021-09-10 19:18:57 +0600

    extract Cup to binance package

---
+++ lib/binance/cup.go
@@ -0,0 +1,47 @@
+package binance
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+type Cup struct {
+	LastUpdateId int         `json:"lastUpdateId"`
+	Bids         [][2]string `json:"bids"`
+	Asks         [][2]string `json:"asks"`
+}
+
+func (c *Cup) cut_data() {
+	c.Bids = c.Bids[:15]
+	c.Asks = c.Asks[:15]
+}
+
+func (c Cup) get_total_asks() string {
+	float_total := c.get_totals(c.Asks)
+	total_ask := fmt.Sprintf("%f", float_total)
+	return total_ask
+}
+
+func (c Cup) get_total_bids() string {
+	float_total := c.get_totals(c.Bids)
+	total_ask := fmt.Sprintf("%f", float_total)
+	return total_ask
+}
+
+func (c Cup) get_totals(data [][2]string) float64 {
+	var total float64
+	for _, val := range data {
+		q, err := strconv.ParseFloat(val[0], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p, err := strconv.ParseFloat(val[1], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += q * p
+	}
+	return total
+}
